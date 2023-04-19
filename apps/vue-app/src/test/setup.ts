0e76ce24fff80a494942c9030545280e55f21662
Nandor Dudas <nandor.dudas@gmail.com>
2023-04-19 11:43:28 +0000

    fix(vue-app): fetch in tests

--- apps/vue-app/src/test/setup.ts
+++ apps/vue-app/src/test/setup.ts
@@ -1,11 +1,17 @@
+// Under Node.js 18 we need cross-fetch
+import { fetch } from 'cross-fetch'
+import 'cross-fetch/polyfill'
+
 import { server } from './mocks/server'
 
 beforeAll(() => {
+  vi.stubGlobal('fetch', fetch)
   server.listen({ onUnhandledRequest: 'error' })
 })
 
 afterAll(() => {
   server.close()
+  vi.unstubAllGlobals()
 })
 
 afterEach(() => {
